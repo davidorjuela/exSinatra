bc2068c712df7933fea224740a6c826e648012d3
davidorjuela <davidorjuel@gmail.com>
2019-06-13 20:45:58 -0500

    Solución reto1 de Sinatra corregida

--- solution.rb
+++ solution.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  if params[:nombre] != nil
-    "<h1>Hola #{params[:nombre]}!</h1>"
-  else
+  if params[:nombre] == nil || params[:nombre]==""
     "<h1>Hola desconocido!</h1>"
+  else  
+    "<h1>Hola #{params[:nombre]}!</h1>"
   end
 end
