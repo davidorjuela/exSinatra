d18f2ddf62b5ec978b4bcf36293906b6f05217e4
davidorjuela <davidorjuel@gmail.com>
2019-06-14 18:24:06 -0500

    ejercicio 2 corregido2

--- solution.rb
+++ solution.rb
@@ -4,6 +4,6 @@ get '/makers/:nombre' do
   if params[:nombre] == nil || params[:nombre]==""
     "<h1>Hola desconocido!</h1>"
   else
-    "<h1>Hola #{params[:nombre]}!</h1>"
+    "<h1>Hola #{params[:nombre].capitalize}!</h1>"
   end
 end
